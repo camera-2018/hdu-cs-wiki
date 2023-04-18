27990c9868f4504def675016b9249f4c5a89e9d7
camera-2018 <2907618001@qq.com>
2023-04-18 14:31:29 +0800

    feat: pdf components

---
+++ components/pdf.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="pdf">
+    <button @click='open'>打开PDF</button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const url = defineProps({
+  url: {
+    type: String,
+    default: ''
+  }
+})
+const open = () => {
+  window.open(url.url)
+}
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  height: 40px;
+  background-color: #409eff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #66b1ff;
+}
+
+.pdf {
+  display: block;
+  margin: 1rem auto;
+}
+</style>
