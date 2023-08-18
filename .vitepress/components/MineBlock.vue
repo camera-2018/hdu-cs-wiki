ea699ec8759b042bc1e64f4f3bc73f65bf19145f
camera-2018 <2907618001@qq.com>
2023-08-19 04:21:05 +0800

    fix: 扫雷小游戏

--- .vitepress/components/MineBlock.vue
+++ .vitepress/components/MineBlock.vue
@@ -62,7 +62,7 @@ function getBlockClass(block) {
       </div>
     </template>
     <template v-else-if="block.revealed">
-      <div v-if="block.mine">
+      <div v-if="block.mine" class="button-block">
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M23 13v-2h-3.07a7.988 7.988 0 0 0-1.62-3.9l2.19-2.17l-1.43-1.43l-2.17 2.19A7.988 7.988 0 0 0 13 4.07V1h-2v3.07c-1.42.18-2.77.74-3.9 1.62L4.93 3.5L3.5 4.93L5.69 7.1A7.988 7.988 0 0 0 4.07 11H1v2h3.07c.18 1.42.74 2.77 1.62 3.9L3.5 19.07l1.43 1.43l2.17-2.19c1.13.88 2.48 1.44 3.9 1.62V23h2v-3.07c1.42-.18 2.77-.74 3.9-1.62l2.17 2.19l1.43-1.43l-2.19-2.17a7.988 7.988 0 0 0 1.62-3.9H23M12 8a4 4 0 0 0-4 4H6a6 6 0 0 1 6-6v2Z"/></svg>
       </div>
       <div v-else class="font-600">
