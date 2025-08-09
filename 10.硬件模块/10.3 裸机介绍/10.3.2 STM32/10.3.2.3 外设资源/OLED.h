284fde54a662320329a58fb637ca2f9f7465ffe6
zhangys001 <3189261171@qq.com>
2025-08-09 23:58:33 +0800

    Add files via upload

---
+++ 10.硬件模块/10.3 裸机介绍/10.3.2 STM32/10.3.2.3 外设资源/OLED.h
@@ -0,0 +1,13 @@
+#ifndef __OLED_H
+#define __OLED_H
+
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char);
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String);
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length);
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowBinNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+
+#endif
